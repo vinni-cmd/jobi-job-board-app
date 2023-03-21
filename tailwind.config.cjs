dc47c24a86b01a77a60e8488cfe79f99bbf17af5
vincentYoung <vinni.xander@gmail.com>
2023-03-21 10:49:33 -0600

    set up fonts and colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,6 +3,25 @@ module.exports = {
   content: [],
   purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
+    extend: {
+      colors: {
+        palette: {
+          100: '#f5f5f5',
+          200: '#e9f2ef',
+          300: '#c4c4c4',
+          500: '#d2f34c',
+          600: '#31795a',
+          800: '#254135',
+          900: '#0f1620',
+        },
+      },
+      fontFamily: {
+        'eb-garamond': ['EB Garamond', 'serif'],
+        gordita: ['Gordita', 'sans-serif'],
+      },
+    },
+  },
+  variants: {
     extend: {},
   },
   plugins: [],
