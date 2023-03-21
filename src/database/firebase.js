df9bf7ab8c9c62fc0f883651329f5db84cd975dd
vincentYoung <vinni.xander@gmail.com>
2023-03-21 09:09:57 -0600

    config firebase

---
+++ src/database/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA3Gz3HX-XHagiGSLkDpfvvvhNS0E4goZ8",
+  authDomain: "jobi-job-board.firebaseapp.com",
+  projectId: "jobi-job-board",
+  storageBucket: "jobi-job-board.appspot.com",
+  messagingSenderId: "746704455412",
+  appId: "1:746704455412:web:a88888cb5e933a939080a1"
+};
+
+// Initialize Firebase
+const firebase = initializeApp(firebaseConfig);
+
+export default firebase;
