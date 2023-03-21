2c80b779838e608e399ff6cbbc62b8d39fa00ade
vincentYoung <vinni.xander@gmail.com>
2023-03-21 17:26:53 -0600

    start structuring and styling header and home banner

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,53 @@
+import { Link } from "react-router-dom"
+
 const Header = () => {
   return (
-    <div>Header</div>
+    <header className=" bg-transparent absolute flex text-secondary-1">
+      <h1 className=" before:content-jobi-icon">jobi</h1>
+      <nav className="flex">
+        <div className=" before:content-category-icon">Category</div>
+        <ul>
+          <li>
+            <Link to={'/'}>
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link to={'/joblist'}>
+              Job
+            </Link>
+          </li>
+          <li>
+            <Link to={'/'}>
+              Explore
+            </Link>
+          </li>
+          <li>
+            <Link to={'/'}>
+              Contact
+            </Link>
+          </li>
+        </ul>
+        <ul>
+          <li>
+            <Link to={'/'}>
+              Post Job
+            </Link>
+          </li>
+          <li>
+            <Link to={'/'}>
+              Login
+            </Link>
+          </li>
+          <li>
+            <Link to={'/'}>
+              Hire Top Talents
+            </Link>
+          </li>
+        </ul>
+      </nav>
+
+    </header>
   )
 }
 
--- src/components/HomeBanner.jsx
+++ src/components/HomeBanner.jsx
@@ -1,6 +1,11 @@
 const HomeBanner = () => {
   return (
-    <div>HomeBanner</div>
+    <section className="bg-primary-1 min-h-screen pt-44">
+      <div className="wrapper">
+        <h2>Find & Hire</h2>
+        <p> Top 3% of expert on jobi.</p>
+      </div>
+    </section>
   )
 }
 
